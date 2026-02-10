83357df398092f4a7618ea21bca39f17be499765
estanislaumateus <estanislaubartolomeu@gmail.com>
2026-02-10 20:28:58 +0100

    feat: componente atualizado

--- reactjs-course/project001/src/App.js
+++ reactjs-course/project001/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import Event3 from './components/Eventos/Event3';
+import Event4 from './components/Eventos/Event4';
 
 function mensagem(){
   alert("Olá, essa mensagem foi dsparada por uma funcção!")
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
   
       <Event3 />
+      <Event4 />
       
     </div>
   );
